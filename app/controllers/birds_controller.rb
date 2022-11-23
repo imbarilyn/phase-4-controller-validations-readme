bf366df1f9d9f3f3b517d9f6ec57c51c862073ef
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-23 15:03:57 +0300

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
 
   # GET /birds
   def index
@@ -9,8 +10,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    bird = Bird.create!(bird_params)
+     render json: bird, status: :created
+  
   end
 
   # GET /birds/:id
@@ -22,8 +24,9 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  
   end
 
   # DELETE /birds/:id
@@ -45,6 +48,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_invalid(invalid)
+    render json: {error: invalid.record.errors.full_messages}, status: 422
   end
 
 end
